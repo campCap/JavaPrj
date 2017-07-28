23c280d2ba8f2d212f82a5fa2b60c1d2ff1aad98
user <user@user-pc>
2017-07-28 14:12:38 +0900

    꼬이꼬이

--- src/seat/ShuffleSeat.java
+++ src/seat/ShuffleSeat.java
@@ -7,6 +7,7 @@ public class ShuffleSeat {
 		Random rand = new Random();
 		double n =  rand.nextDouble();
 		String[][] names = {{"차명건",""}, {"개나리",""}, {"목대승",""}, {"차형주",""},{"백지원",""}, {"이종혁",""}};
-		System.out.println(names[0].hashCode());
+		System.out.println(names.hashCode());
 	}
+	
 }
