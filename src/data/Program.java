600b2dac8146169770a14f41431f07bc5744dcbb
aaaa <aaaa@user83>
2017-08-07 16:51:58 +0900

    08.07 맹구

--- src/data/Program.java
+++ src/data/Program.java
@@ -9,12 +9,12 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Scanner scan= new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.print("찾고자 하는 값을  입력하라 : ");
 		String str = scan.nextLine();
-		
+
 		String url = "jdbc:oracle:thin:@211.238.142.251:1521:orcl";
-		String sql = "SELECT * FROM NOTICE WHERE TITLE LIKE '%"+str+"%'";
+		String sql = "SELECT * FROM NOTICE WHERE TITLE LIKE '%" + str + "%'";
 		// 드라이버 로드
 		Class.forName("oracle.jdbc.OracleDriver");
 		// 연결 / 인증
@@ -23,9 +23,11 @@ public class Program {
 		Statement st = con.createStatement();
 		// 결과가져오기
 		ResultSet rs = st.executeQuery(sql);
-
-		while (rs.next())
-			System.out.printf("ID : %s   TITLE : %s \n" ,rs.getString("ID"), rs.getString("title"));
+		if (rs.next() == false)
+			System.out.println("일치하는 값이 없습니다");
+		else
+			while (rs.next())
+				System.out.printf("ID : %s   TITLE : %s \n", rs.getString("ID"), rs.getString("title"));
 
 		rs.close();
 		st.close();
